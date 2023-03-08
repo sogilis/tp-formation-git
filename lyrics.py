bc2c962ba19644c6907d6ba4bf0421b0c1afe4a5
Aurélien Carlier <aurelien@sogilis.com>
2023-03-08 09:39:26 +0100

    add lyrics and tests

---
+++ lyrics.py
@@ -0,0 +1,13 @@
+def first_verse() -> list[str]:
+    return [
+        "When I find myself in times of trouble, Mother Mary comes to me",
+        "Speaking words of wisdom, let it be",
+        "And in my hour of darkness she is standing right in front of me",
+        "Speaking words of wisdom, let it be",
+    ]
+
+def second_verse() -> list[str]:
+    return [
+        "Let it be, let it be, let it be, let it be",
+        "Whisper words of wisdom, let it be",
+    ]
