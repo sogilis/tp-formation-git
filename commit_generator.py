f9d9ef8a428c4944b59eb162a4b243338cd23127
Aurélien Carlier <aurelien@sogilis.com>
2023-03-08 10:00:03 +0100

    add script for commit generation

---
+++ commit_generator.py
@@ -0,0 +1,20 @@
+#! /usr/bin/python3.9
+
+import os
+
+commit_count_start = 1
+commit_count_end = 3
+
+# boucle pour générer 10 commits
+for i in range(commit_count_start, commit_count_end):
+    commit_message = f"Suspicious commit #{i}"
+    
+    # ajouter un fichier avec un nom aléatoire et un contenu aléatoire
+    filename = 'commit_number'
+    with open(filename, 'w') as f:
+        f.write(str(i))
+    
+    # ajouter les modifications au suivi de version
+    os.system('git add ' + filename)
+    # faire un commit avec le message aléatoire
+    os.system('git commit -m "' + commit_message + '"')
