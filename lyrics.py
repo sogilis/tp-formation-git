7e103269547b82241b920f80ea4c3db49151ae35
Mathieu RIGARD <mathieu.rigard@sogilis.com>
2024-05-29 10:07:36 +0200

    WIP

--- lyrics.py
+++ lyrics.py
@@ -2,7 +2,12 @@ def sing() -> list[str]:
     raise Exception("sing not implemented :(")
 
 def first_verse() -> list[str]:
-    raise Exception("firt_verse not implemented :(")
+    return [
+            "When I find myself in times of trouble, Mother Mary comes to me",
+            "Speaking words of wisdom, let it be",
+            "And in my hour of darkness she is standing right in front of me",
+            "Speaking words of wisdom, let it be",
+        ]
 
 def second_verse() -> list[str]:
     raise Exception("second_verse not implemented :(")
